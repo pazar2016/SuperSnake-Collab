87c62dffb96781c9ee93d521f0474abaa03b9655
ahuesing <student@ahuesing.local>
2016-04-14 10:27:42 -0500

    brick

--- Super Snake/GameViewController.swift
+++ Super Snake/GameViewController.swift
@@ -8,28 +8,21 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UICollisionBehaviorDelegate
+{
+    var snake = UIView()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        snake = UIView(frame: CGRectMake(view.center.x - 30, 20, 60, 30))
+        snake.backgroundColor = UIColor.magentaColor()
+        view.addSubview(snake)
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
